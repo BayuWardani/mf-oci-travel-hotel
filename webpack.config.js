22744934b1a1e3d6db44b2ccea20153969409fca
Bayu Wardani <bayuwardani@Bayus-MacBook-Air.local>
2024-03-02 10:46:15 +0700

    oci

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "oci",
+    projectName: "travel-hotel",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return merge(defaultConfig, {
+    module:{
+      rules:[
+        {
+          test: /\.(css)$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+    }
+  });
+};
